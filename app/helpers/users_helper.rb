5c55eeb8f614fc293262cf7147666b380e540443
Your Name <takayamac@icloud.com>
2018-08-18 14:35:13 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #gravatarのidを設定
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}" #gravatorのurlにidを紐づける
+    image_tag(gravatar_url, alt: user.name, class: "gravatar") #ユーザーの画像を取得
+  end
 end
