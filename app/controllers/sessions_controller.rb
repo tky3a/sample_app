6d4baf50a12ca3c5b313bbab4e7211a37659a547
Your Name <takayamac@icloud.com>
2018-08-18 17:23:27 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)#loginフォームに対応したsessionを取得
+    if user && user.authenticate(params[:session][:password])#emailとpassword
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "正しく入力してください"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+
+end
